16f0d739d42f02653f797b1c68886c3a7ecbaeaa
Scott Richards <scott.srichards@gmail.com>
2012-06-17 14:01:52 -0700

    Got pull down menu working to select sites.

---
+++ selectMenu.php
@@ -0,0 +1,32 @@
+<?php
+
+	require_once "sql/config.php";
+	define ('DEBUG', false);
+	
+  function selectMenu($connection, $tableName, $attributeName, $pulldownName) {
+		$sqlDistinctQuery = "SELECT DISTINCT $attributeName from $tableName";
+		if (DEBUG) print "SQL: $sqlDistinctQuery\n<br />";
+		
+		$result = @ mysql_query($sqlDistinctQuery, $connection);
+		print "Num Rows: " . mysql_num_rows($result) . "\n<br />";
+		if (!$result) {
+			print "Error with sql Statement: $sqlDistinctQuery\n";
+			die;
+		}
+		
+		// output select pull down menu with specified name	
+		print "\n<select name=\"{$pulldownName}\">";
+		// retreive each row from results of query
+		while ($row = @ mysql_fetch_array($result)) {
+				if (DEBUG) print "$row[$attributeName]\n<br />"; 
+			print "\n\t<option value=\"{$row[$attributeName]}\">{$row[$attributeName]}</option>";
+		}
+		print "\n</select>";
+	}
+
+	// create a connection to mysql
+	$connection = mysql_connect(SQL_SERVER,SQL_USER);
+	// select the stumbleupon database
+	mysql_select_db(SQL_DATABASE,$connection);
+	selectMenu($connection,SQL_VIEWS_TABLE,"siteName","siteNameSelectMenu");
+?>
